d4498ab2f4d64852aba1db600c11ce8a11cf1640
galileo <galileo@Imaginarium>
2015-04-29 00:20:25 -0500

    Se realiza la red de Erdos-Renyi

--- src/prototipo/Prototipo.java
+++ src/prototipo/Prototipo.java
@@ -5,6 +5,9 @@
  */
 package prototipo;
 
+import java.awt.Toolkit;
+import java.math.BigInteger;
+
 /**
  *
  * @author jedisson.tapias
@@ -13,14 +16,18 @@ public class Prototipo {
 
     static Agente[] agentes;
     static Capa[] capas;
+    static double probabilidad;
     
     public static void main(String[] args) {
         generarPrototipo();
         mostrarPrototipo();
+        generarParametroErdos(0.9,8,100);
+        generarRed();
+        mostrarPrototipo();
     }
     
     public static void generarPrototipo(){
-        int cantidadDeAgentes=5;
+        int cantidadDeAgentes=100;
         int cantidadDeCapas=3;
         //----------------------------------------------------
         //Agentes
@@ -74,7 +81,56 @@ public class Prototipo {
             System.out.print("\n>> Capa "+capas[a].getNombre()+" --------------");
             for(int b=0;b<capas[a].getConexiones().size();b++){
                 Conexion c=(Conexion)capas[a].getConexionesI(b);
+
                 System.out.print("\n "+c.getOrigen().getNombre()+"\t"+c.getDestino().getNombre()+"\t"+c.getCapa().getNombre()+"\t"+c.getEsActiva());
+            }
+        }
+    }
+    
+    private static long factorial(long x){
+        
+        if(x<1){
+            return 1;
+        }
+        else{
+            return x*factorial(x-1);
+        }
+                
+        
+    }
+    private static long combinatoria(long x,int y){
+        return (x*(x-1))/y;
+    }
+    
+    private static void generarParametroErdos(double p,int m,int n){
+        //Erdos-Renyi Network
+        //Statiscal Random Network
+        //p: parameter  m: edged n: nodes
+        //BigInteger n= BigInteger.valueOf(_n);
+        //double combinatoria= factorial(n)/(factorial()*factorial(BigInteger.valueOf(2)));
+        //System.out.print("\n\n>>factorial(+"+n+"): "+factorial(n)+"\n");
+        double m1=combinatoria(n,2)*p;
+        System.out.print("\n\n>>Aristas: "+m1+"\n");
+        System.out.print("\n\n>>Combinatoria: "+combinatoria(n,2)+"\n");
+        System.out.print("\n\n>>Combinatoria - Aristas: "+ (combinatoria(n,2) - m1 )+"\n");
+        System.out.print("\n\n>>pow(1-"+p+")^Combinatoria - Aristas: "+ (Math.pow(1-p, (combinatoria(n,2)- m1 )))+"\n");
+        probabilidad = Math.pow(p,  m1)*(Math.pow(1- p, (combinatoria(n,2)- m1 )));      
+        System.out.print("\n\n>>Probabilidad: "+probabilidad+"\n\n");
+        probabilidad=0.5;
+        
+        
+    }
+
+    private static void generarRed() {
+        Toolkit tk = Toolkit.getDefaultToolkit (); 
+        for(int a=0;a< capas.length;a++){
+            for(int b=0;b<capas[a].getConexiones().size();b++){
+                Conexion c=(Conexion)capas[a].getConexionesI(b);
+                if(Math.random()<=probabilidad){
+                    c.setEsActiva(true);
+                    //tk.beep();
+                }
+                
             }
         }
     }
